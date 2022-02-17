f940a6995784652ecde71fab7bd3a84af9129f27
Matheus Mazeto <mgmazeto@gmail.com>
2022-02-17 18:45:31 -0300

    add tailwind and pm2

---
+++ app/root.tsx
@@ -0,0 +1,41 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "remix";
+import type { MetaFunction } from "remix";
+
+import globalStyles from "./styles/global.css"
+import tailwindStyles from "./styles/tailwind.css"
+
+export function links() {
+  return [{ rel: "stylesheet", href: globalStyles },
+  { rel: "stylesheet", href: tailwindStyles }
+]
+}
+
+export const meta: MetaFunction = () => {
+  return { title: "New Remix App" };
+};
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-400 bg-slate-800">
+      Hello world!
+    </h1>
+  );
+}
